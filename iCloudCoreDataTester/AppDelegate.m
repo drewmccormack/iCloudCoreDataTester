8858732eab38e7e97e6fd6bbb2935fac1a3cd8a9
Drew McCormack <drewmccormack@mac.com>
2012-05-28 15:31:36 +0200

    Minor improvements, including better handing of failures when migrating store data into iCloud.

--- iCloudCoreDataTester/AppDelegate.m
+++ iCloudCoreDataTester/AppDelegate.m
@@ -72,13 +72,16 @@ static NSString * const TeamIdentifier = @"XXXXXXXXXX";
 -(IBAction)addNote:(id)sender
 {
     id newNote = [NSEntityDescription insertNewObjectForEntityForName:@"Note" inManagedObjectContext:self.managedObjectContext];
+    
     id newFacet = [NSEntityDescription insertNewObjectForEntityForName:@"Facet" inManagedObjectContext:self.managedObjectContext];
     [newFacet setValue:newNote forKey:@"note"];
     newFacet = [NSEntityDescription insertNewObjectForEntityForName:@"Facet" inManagedObjectContext:self.managedObjectContext];
     [newFacet setValue:newNote forKey:@"note"];
+    
     id newPermutation = [NSEntityDescription insertNewObjectForEntityForName:@"Permutation" inManagedObjectContext:self.managedObjectContext];
     [newPermutation setValue:newFacet forKey:@"facet"];
     [newPermutation setValue:newNote forKey:@"note"];
+    
     id newSchedule = [NSEntityDescription insertNewObjectForEntityForName:@"ChildSchedule" inManagedObjectContext:self.managedObjectContext];
     [newPermutation setValue:newSchedule forKey:@"schedule"];
 }
@@ -87,14 +90,14 @@ static NSString * const TeamIdentifier = @"XXXXXXXXXX";
 {
     id note = [[notesController selectedObjects] lastObject];
     if ( !note || notesController.selectedObjects.count > 1 ) return;
-    [self.managedObjectContext processPendingChanges];
+    
     id newSchedule = [NSEntityDescription insertNewObjectForEntityForName:@"ChildSchedule" inManagedObjectContext:self.managedObjectContext];
     [newSchedule setValue:[[NSProcessInfo processInfo] globallyUniqueString] forKey:@"title"];
+    
     id permutation = [[note valueForKey:@"permutations"] anyObject];
     id existingSchedule = [permutation valueForKey:@"schedule"];
     if ( existingSchedule ) [self.managedObjectContext deleteObject:existingSchedule];
     [permutation setValue:newSchedule forKey:@"schedule"];
-    [self.managedObjectContext processPendingChanges];
 }
 
 #pragma mark Core Data Stack
@@ -152,13 +155,18 @@ static NSString * const TeamIdentifier = @"XXXXXXXXXX";
         return nil;
     }
     
+    // Use cloud storage if iCloud is enabled, and the user default is set to YES.
     NSURL *storeURL = self.cloudStoreURL;
     BOOL usingCloudStorage = [[NSUserDefaults standardUserDefaults] boolForKey:UsingCloudStorageDefault];
     usingCloudStorage &= storeURL != nil;
+    
+    // Basic options
     NSMutableDictionary *options = [NSMutableDictionary dictionaryWithObjectsAndKeys:
                                     (id)kCFBooleanTrue, NSMigratePersistentStoresAutomaticallyOption, 
                                     (id)kCFBooleanTrue, NSInferMappingModelAutomaticallyOption, 
                                     nil];
+    
+    // iCloud options
     if ( usingCloudStorage ) {
         [options addEntriesFromDictionary:[NSDictionary dictionaryWithObjectsAndKeys:
                                            MCCloudMainStoreFileName, NSPersistentStoreUbiquitousContentNameKey,
@@ -184,12 +192,11 @@ static NSString * const TeamIdentifier = @"XXXXXXXXXX";
         }
     } else {
         if (![[properties objectForKey:NSURLIsDirectoryKey] boolValue]) {
-            // Customize and localize this error.
             NSString *failureDescription = [NSString stringWithFormat:@"Expected a folder to store application data, found a file (%@).", [applicationFilesDirectory path]];
             
             NSMutableDictionary *dict = [NSMutableDictionary dictionary];
             [dict setValue:failureDescription forKey:NSLocalizedDescriptionKey];
-            error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:101 userInfo:dict];
+            error = [NSError errorWithDomain:@"MCErrorDomain" code:101 userInfo:dict];
             
             [[NSApplication sharedApplication] presentError:error];
             return nil;
@@ -204,6 +211,7 @@ static NSString * const TeamIdentifier = @"XXXXXXXXXX";
     }
     __persistentStoreCoordinator = coordinator;
     
+    // Register as observer for iCloud merge notifications
     [[NSNotificationCenter defaultCenter] removeObserver:self];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(persistentStoreCoordinatorDidMergeCloudChanges:) name:NSPersistentStoreDidImportUbiquitousContentChangesNotification object:coordinator];
     
@@ -240,15 +248,16 @@ static NSString * const TeamIdentifier = @"XXXXXXXXXX";
     NSError *error;
     NSURL *storeURL = self.localStoreURL;
     NSURL *oldStoreURL = [[self applicationFilesDirectory] URLByAppendingPathComponent:@"OldStore"];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
     
     // If there is no local store, no need to migrate
-    if ( ![[NSFileManager defaultManager] fileExistsAtPath:storeURL.path] ) return;
+    if ( ![fileManager fileExistsAtPath:storeURL.path] ) return;
         
     // Remove any existing old store file left over from a previous migration
-    [[NSFileManager defaultManager] removeItemAtURL:oldStoreURL error:NULL];
+    [fileManager removeItemAtURL:oldStoreURL error:NULL];
     
     // Move existing local store aside
-    if ( ![[NSFileManager defaultManager] moveItemAtURL:storeURL toURL:oldStoreURL error:&error] ) {
+    if ( ![fileManager moveItemAtURL:storeURL toURL:oldStoreURL error:&error] ) {
         [[NSApplication sharedApplication] presentError:error];
         return;
     }
@@ -267,13 +276,18 @@ static NSString * const TeamIdentifier = @"XXXXXXXXXX";
     NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:self.managedObjectModel];
     id oldStore = [coordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:oldStoreURL options:localOnlyOptions error:&error];
     if ( !oldStore ) {
+        // Move back the old store and present the error
+        [fileManager moveItemAtURL:oldStoreURL toURL:storeURL error:NULL];
         [[NSApplication sharedApplication] presentError:error];
         return;
     }
         
     // Migrate existing (old) store to new store
     if ( ![coordinator migratePersistentStore:oldStore toURL:storeURL options:cloudOptions withType:NSSQLiteStoreType error:&error] ) {
+        [fileManager removeItemAtURL:storeURL error:NULL];
+        [fileManager moveItemAtURL:oldStoreURL toURL:storeURL error:NULL];
         [[NSApplication sharedApplication] presentError:error];
+        return;
     }
     else {
         [[NSFileManager defaultManager] removeItemAtURL:oldStoreURL error:NULL];
@@ -290,13 +304,12 @@ static NSString * const TeamIdentifier = @"XXXXXXXXXX";
     NSURL *storeURL = self.cloudStoreURL;
     if ( !storeURL ) return;
     [coordinator coordinateWritingItemAtURL:storeURL options:NSFileCoordinatorWritingForDeleting error:NULL byAccessor:^(NSURL *newURL) {
-        [[NSFileManager defaultManager] removeItemAtURL:newURL error:nil];
+        [[NSFileManager defaultManager] removeItemAtURL:newURL error:NULL];
     }];
 }
 
 -(void)persistentStoreCoordinatorDidMergeCloudChanges:(NSNotification *)notification
 {
-    // Notification contains object ids. The merge method expects objects, so need to convert.
     [self.managedObjectContext performBlock:^{        
         [self.managedObjectContext mergeChangesFromContextDidSaveNotification:notification]; 
         
@@ -310,17 +323,14 @@ static NSString * const TeamIdentifier = @"XXXXXXXXXX";
 -(NSManagedObjectContext *)managedObjectContext
 {
     if ( !stackIsSetup ) return nil;
-    
-    if (__managedObjectContext) {
-        return __managedObjectContext;
-    }
+    if (__managedObjectContext) return __managedObjectContext;
     
     NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
     if (!coordinator) {
         NSMutableDictionary *dict = [NSMutableDictionary dictionary];
         [dict setValue:@"Failed to initialize the store" forKey:NSLocalizedDescriptionKey];
         [dict setValue:@"There was an error building up the data file." forKey:NSLocalizedFailureReasonErrorKey];
-        NSError *error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
+        NSError *error = [NSError errorWithDomain:@"MCErrorDomain" code:9999 userInfo:dict];
         [[NSApplication sharedApplication] presentError:error];
         return nil;
     }
@@ -350,9 +360,7 @@ static NSString * const TeamIdentifier = @"XXXXXXXXXX";
 
 -(NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender
 {    
-    if (!__managedObjectContext) {
-        return NSTerminateNow;
-    }
+    if (!__managedObjectContext) return NSTerminateNow;
     
     if (![[self managedObjectContext] commitEditing]) {
         NSLog(@"%@:%@ unable to commit editing to terminate", [self class], NSStringFromSelector(_cmd));
@@ -365,12 +373,8 @@ static NSString * const TeamIdentifier = @"XXXXXXXXXX";
     
     NSError *error = nil;
     if (![[self managedObjectContext] save:&error]) {
-
-        // Customize this code block to include application-specific recovery steps.              
         BOOL result = [sender presentError:error];
-        if (result) {
-            return NSTerminateCancel;
-        }
+        if (result) return NSTerminateCancel;
 
         NSString *question = NSLocalizedString(@"Could not save changes while quitting. Quit anyway?", @"Quit without saves error question message");
         NSString *info = NSLocalizedString(@"Quitting now will lose any changes you have made since the last successful save", @"Quit without saves error question info");
@@ -384,9 +388,7 @@ static NSString * const TeamIdentifier = @"XXXXXXXXXX";
 
         NSInteger answer = [alert runModal];
         
-        if (answer == NSAlertAlternateReturn) {
-            return NSTerminateCancel;
-        }
+        if (answer == NSAlertAlternateReturn) return NSTerminateCancel;
     }
 
     return NSTerminateNow;
